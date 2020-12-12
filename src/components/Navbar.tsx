9e6e0859aaf1767f87a00ae06d72fa9bf3476d9a
BevilKunG <devilbevilgamer@gmail.com>
2020-12-12 14:27:47 +0700

    edit navbar sticky top

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,11 @@ import { Link } from 'gatsby'
 
 const Navbar: FC = () => {
   return (
-    <div className="w-screen h-16 bg-gray-900 flex flex-row justify-start items-center px-8">
+    <div className="sticky top-0 z-10 w-screen h-10 bg-gray-900 flex flex-row justify-start items-center px-8">
       <h1>
         <Link
           to="/"
-          className="text-3xl text-white font-bold hover:no-underline"
+          className="text-xl text-white font-bold hover:no-underline"
         >
           Bevil Blog
         </Link>
